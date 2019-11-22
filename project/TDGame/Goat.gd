d216e83cca10c4bc02bc80b37ee18f9da039401d
Christoph Heinen <heinen@fachschaft.info>
2019-11-23 00:02:17 +0100

    wellen csv

--- project/TDGame/Goat.gd
+++ project/TDGame/Goat.gd
@@ -7,12 +7,17 @@ extends KinematicBody2D
 export var spawnpoint = Vector2(1,1)
 export var destination = Vector2(500,150)
 
+signal win
+
 export var SPEED = 100
 var path = []
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var navigation = get_parent().get_node("Navigation2D")
 	path = navigation.get_simple_path(position, destination, false)
+
+func win():
+	emit_signal("win")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -22,11 +27,12 @@ func _process(delta):
 	if(path.size() > 0):
 		
 		var next_position = path[0]
-		print(next_position)
-		if abs(position.x - next_position.x) + abs(position.y - next_position.y) <=1:
+		if abs(position.x - next_position.x) + abs(position.y - next_position.y) <=8:
 			path.remove(0)
 		var direction = next_position-position 
 		direction = direction.normalized()
 		
 		move_and_slide(direction * SPEED,Vector2(1,0))
-		
+	else:
+		set_process(false)
+		win()
---
+++ project/TDGame/TDGame.gd
@@ -0,0 +1,44 @@
+extends Node2D
+
+var scene = load("res://TDGame/Goat.tscn")
+
+export(String, FILE) var wavesTXT
+
+var waves = []
+
+var threshold = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var groupname=""
+	var file = File.new()
+	if file.file_exists(wavesTXT):
+		file.open(wavesTXT, file.READ)
+		while !file.eof_reached():
+			var csv = file.get_csv_line (" ")
+			waves.push_back(csv)
+		file.close()
+	print(waves)
+	set_process(true)
+
+func _process(delta):
+	threshold -= delta
+	if threshold < 0:
+		var befehl = waves.pop_front()
+		if waves.size() > 0:
+			if befehl[0] == "spawn":
+				spawn_goat()
+			elif befehl[0] == "pause":
+				threshold += int(befehl[1])
+		else:
+			print('keine weiteren Schafe')
+			set_process(false)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func spawn_goat():
+	var goat = scene.instance()
+	add_child(goat)
+	
