31f2aeaec52e6640759cdcdf4e029192d43ce22a
Christoph Heinen <heinen@fachschaft.info>
2019-11-23 00:36:59 +0100

    gui 1. versuch

---
+++ project/GUI.gd
@@ -0,0 +1,17 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func set_geld(value):
+	$HBoxContainer/Geld/Value.text = value
+
+func set_futter(value):
+	$HBoxContainer/Futter/Value.text = value
+
+func set_energie(value):
+	$HBoxContainer/Energie/Value.text = value
