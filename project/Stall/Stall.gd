4c0fc1e4bf321a516d59ba1098d4a23128c5542d
Tarmandan <s6ssklei@uni-bonn.de>
2019-11-23 11:37:23 +0100

    stall

---
+++ project/Stall/Stall.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var schaf = 1
+var ziege = 0
+var feld = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func add_schaf(schaf):
+	schaf += 1
+
+func add_ziege(ziege):
+	ziege += 1
+
+func add_feld(feld):
+	feld += 1
+
