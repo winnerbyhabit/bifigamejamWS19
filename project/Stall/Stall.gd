f5c7dc06e51bf2d6b8bc0144cf308b34fb02f1f2
Christoph Heinen <heinen@fachschaft.info>
2019-11-23 17:19:18 +0100

    field placement

--- project/Stall/Stall.gd
+++ project/Stall/Stall.gd
@@ -12,7 +12,10 @@ export var anzahl_kacheln = Vector2(8,9)
 
 export var gridsize = 64
 
-var field_cursor = load("res://assets/Tower_1.png")
+var field_cursor = load("res://assets/T22.png")
+
+var field_id = 1
+var buyable_field_id = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -30,40 +33,27 @@ func add_goat(goat):
 func _input(event):
 	if fieldplacement_active:
 		if Input.is_action_pressed("left_click"):
-			var click_position = event.position
+			var click_position = event.position - self.position
 			click_position.x = floor(click_position.x / gridsize)
 			click_position.y = floor(click_position.y / gridsize)
 			if click_position.x >= 0 and click_position.x < anzahl_kacheln.x and click_position.y >= 5 and click_position.y < anzahl_kacheln.y: 
-				place_field(click_position)				
+				place_field(click_position)
 
 func activate_field_placement():
 	fieldplacement_active = true
 	Input.set_custom_mouse_cursor(field_cursor,0,Vector2(gridsize/2,gridsize/2))
 
 func place_field(position):
+	print('place field: ' ,position)
 	if field_placement_possible(position):
 		fieldplacement_active = false
 		Input.set_custom_mouse_cursor(null,Input.CURSOR_ARROW)
-		#var tower = tower_scene.instance()
-		#field.position = position * Vector2(gridsize,gridsize) + Vector2(gridsize/2,gridsize/2)
-		#tower.show_tower_range(false)
-		#add_child(tower)
 		field += 1
-		get_node("TileMap").set_cell(position, 0)
+		$TileMap.set_cellv(position, field_id)
 
 func field_placement_possible(position):
-	#prüfe ob feld/ muss noch angepasst werden and neues tileset
-	if $Stall/TileMap.get_cell(position.x,position.y) != 0:
+	if $TileMap.get_cellv(position) == buyable_field_id:
 		return true
-	#prüfung für feldteil des rechten felds oder true/fale umdrehen und auf brache prüfen
-	
-	#prüffe ob anderer Turm
-	#var children = self.get_children()
-	#for c in children:
-	#	if c.is_in_group("tower"):
-	#		print(c.position,position* Vector2(gridsize,gridsize))
-	#		if c.position == position * Vector2(gridsize,gridsize) + Vector2(gridsize/2,gridsize/2):
-	#			return false
-	
 	return false
+	
 
